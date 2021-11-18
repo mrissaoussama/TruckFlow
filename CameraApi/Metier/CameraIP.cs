ad4a854d9d3a75668db4d5ee3fc98bf1e7598e9f
oussama <mrissaoussama@gmail.com>
2021-11-18 17:53:36 +0100

    dao

---
+++ CameraApi/Metier/CameraIP.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CameraApi.Metier
+{
+    public class CameraIP
+    {
+        public CameraIP() { }
+        public byte[] GetPhoto()
+        {
+            var rand = new Random();
+            var files = Directory.GetFiles("images");
+            return (File.ReadAllBytes(files[rand.Next(files.Length)]));
+        }
+    }
+}
---
+++ DAO/IDAO/IDAOEvent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TruckFlowDomain;
+
+namespace DAO.IDAO
+{
+   public interface IDAOEvent
+    {
+        List<Event>Get
+    }
+}
